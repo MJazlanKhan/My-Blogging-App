abbde02bff10e4c15363410894165c175f3cd200
Muhammad Jazlan Khan <142224933+MJazlanKhan@users.noreply.github.com>
2023-10-29 02:50:39 -0700

    Update Home.js

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
   useEffect(() => {
     const fetchBlogPosts = async () => {
       try {
-        const response = await axios.get('http://localhost:9000/api/v1/posts');
+        const response = await axios.get('https://my-blogging-app-server.vercel.app/api/v1/posts');
         if (response.status === 200) {
           const data = response.data;
           setBlogPosts(data);
@@ -30,7 +30,7 @@ function Home() {
         {blogPosts.map(blogPost => (
           <div className='post' key={blogPost._id}>
             <h2>{blogPost.title}</h2>
-            <img src={`http://localhost:9000${blogPost.imageURL}`} alt="Image" />
+            <img src={`https://my-blogging-app-server.vercel.app${blogPost.imageURL}`} alt="Image" />
             <p>Author: {blogPost.author}</p>
             <p>{blogPost.summary}</p>
             <Button type='primary'>
